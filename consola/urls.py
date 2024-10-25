280c1bda266d7f3a68cae2b732fe2352b5118617
bioqu <lpereirc@gmail.com>
2024-10-25 01:14:55 -0300

    añadidas web de navbar

--- consola/urls.py
+++ consola/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.indice, name = "consola-indice"),
     path("miembros/", views.usuarios_lab, name = "consola-usuarios_lab"),
-    
+    path("reactivos/", views.reactivos, name = "consola-reactivos"),
+    path("reactivos/ingresar/", views.ingresar_reactivos, name = "consola-ingresar_reactivos"),
+    path("reactivos/editar/", views.editar_reactivos, name = "consola-editar_reactivos"),    
+    path("experimentos/", views.experimentos, name = "consola-experimentos"),
 ]
--- consola/views.py
+++ consola/views.py
@@ -6,4 +6,15 @@ def indice(request):
 
 def usuarios_lab(request):
     return render(request, "consola/usuarios_lab.html")
-  
+ 
+def reactivos(request):
+    return render(request, "consola/reactivos.html") 
+
+def ingresar_reactivos(request):
+    return render(request, "consola/ingresar_reactivos.html") 
+
+def editar_reactivos(request):
+    return render(request, "consola/editar_reactivos.html")  
+
+def experimentos(request):
+    return render(request, "consola/experimentos.html")  
